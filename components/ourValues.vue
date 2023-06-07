b4848aa2855095f7568a600127ba8de46a48d0bd
Max <max.lepan@laposte.net>
2023-06-07 10:12:51 +0200

    FIX [Style] Fixed values width

--- components/ourValues.vue
+++ components/ourValues.vue
@@ -6,7 +6,7 @@
   <section class="lg:p-24 md:p-12 sm:p-6">
     <h2 class="font-title font-bold lg:text-5xl md:text-4xl sm:text-3xl text-2xl mb-10">Nos valeurs</h2>
     <div class="grid sm:grid-cols-3 grid-rows-1 sm:gap-0 gap-5 justify-evenly">
-      <div class="flex flex-col items-center justify-start justify-self-start lg:w-56 md:w-44 sm:w-36 w-56">
+      <div class="flex flex-col items-center justify-start justify-self-start lg:w-60 md:w-44 sm:w-36 w-60">
         <img alt="Pedagogy icon" class="lg:h-32 h-24" src="../assets/icons/pedagogie.svg"/>
         <h3 class="font-title font-bold lg:text-4xl md:text-3xl sm:text-2xl text-xl text-center lg:mt-5 md:mt-4 sm:mt-3 mt-2">
           Pédagogie</h3>
@@ -17,7 +17,7 @@
           petites
           bêtes</p>
       </div>
-      <div class="flex flex-col items-center justify-start justify-self-center lg:w-56 md:w-44 sm:w-36 w-56">
+      <div class="flex flex-col items-center justify-start justify-self-center lg:w-60 md:w-44 sm:w-36 w-60">
         <img alt="Pedagogy icon" class="lg:h-32 h-24" src="../assets/icons/compassion.svg"/>
         <h3 class="font-title font-bold lg:text-4xl md:text-3xl sm:text-2xl text-xl text-center lg:mt-5 md:mt-4 sm:mt-3 mt-2">
           Compassion</h3>
@@ -25,7 +25,7 @@
           un attrait et une
           sympathie envers les petites bêtes</p>
       </div>
-      <div class="flex flex-col items-center justify-start justify-self-end lg:w-56 md:w-44 sm:w-36 w-56">
+      <div class="flex flex-col items-center justify-start justify-self-end lg:w-60 md:w-44 sm:w-36 w-60">
         <img alt="Pedagogy icon" class="lg:h-32 h-24" src="../assets/icons/proximite.svg"/>
         <h3 class="font-title font-bold lg:text-4xl md:text-3xl sm:text-2xl text-xl text-center lg:mt-5 md:mt-4 sm:mt-3 mt-2">
           Proximité</h3>
