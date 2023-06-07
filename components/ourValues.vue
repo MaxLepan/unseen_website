616e449da9671b63709dd8401a75a5ddbd301a3a
Max <max.lepan@laposte.net>
2023-06-07 10:09:52 +0200

    ADD [Style] Added our values style

--- components/ourValues.vue
+++ components/ourValues.vue
@@ -3,23 +3,36 @@
 </script>
 
 <template>
-  <section class="p-24">
-    <h2 class="font-title font-bold text-5xl mb-10">Nos valeurs</h2>
-    <div class="grid grid-cols-3 justify-evenly	">
-      <div class="justify-center">
-        <img alt="Pedagogy icon" src="../assets/icons/pedagogie.svg"/>
-        <h3>Pédagogie</h3>
-        <p>Partage de connaissances et d'informations diverses sur les petites bêtes</p>
+  <section class="lg:p-24 md:p-12 sm:p-6">
+    <h2 class="font-title font-bold lg:text-5xl md:text-4xl sm:text-3xl text-2xl mb-10">Nos valeurs</h2>
+    <div class="grid sm:grid-cols-3 grid-rows-1 sm:gap-0 gap-5 justify-evenly">
+      <div class="flex flex-col items-center justify-start justify-self-start lg:w-56 md:w-44 sm:w-36 w-56">
+        <img alt="Pedagogy icon" class="lg:h-32 h-24" src="../assets/icons/pedagogie.svg"/>
+        <h3 class="font-title font-bold lg:text-4xl md:text-3xl sm:text-2xl text-xl text-center lg:mt-5 md:mt-4 sm:mt-3 mt-2">
+          Pédagogie</h3>
+        <p class="font-body lg:text-2xl md:text-xl sm:text-base text-sm text-center lg:mt-7 md:mt-5 sm:mt-3 mt-1">
+          Partage de connaissances et
+          d'informations
+          diverses sur les
+          petites
+          bêtes</p>
       </div>
-      <div>
-        <img alt="Pedagogy icon" src="../assets/icons/compassion.svg"/>
-        <h3>Compassion</h3>
-        <p>Amener un attrait et une sympathie envers les petites bêtes</p>
+      <div class="flex flex-col items-center justify-start justify-self-center lg:w-56 md:w-44 sm:w-36 w-56">
+        <img alt="Pedagogy icon" class="lg:h-32 h-24" src="../assets/icons/compassion.svg"/>
+        <h3 class="font-title font-bold lg:text-4xl md:text-3xl sm:text-2xl text-xl text-center lg:mt-5 md:mt-4 sm:mt-3 mt-2">
+          Compassion</h3>
+        <p class="font-body lg:text-2xl md:text-xl sm:text-base text-sm text-center lg:mt-7 md:mt-5 sm:mt-3 mt-1">Amener
+          un attrait et une
+          sympathie envers les petites bêtes</p>
       </div>
-      <div>
-        <img alt="Pedagogy icon" src="../assets/icons/proximite.svg"/>
-        <h3>Proximité</h3>
-        <p>Discours acceuillants et informels pour mieux comprendre notre environnement</p>
+      <div class="flex flex-col items-center justify-start justify-self-end lg:w-56 md:w-44 sm:w-36 w-56">
+        <img alt="Pedagogy icon" class="lg:h-32 h-24" src="../assets/icons/proximite.svg"/>
+        <h3 class="font-title font-bold lg:text-4xl md:text-3xl sm:text-2xl text-xl text-center lg:mt-5 md:mt-4 sm:mt-3 mt-2">
+          Proximité</h3>
+        <p class="font-body lg:text-2xl md:text-xl sm:text-base text-sm text-center lg:mt-7 md:mt-5 sm:mt-3 mt-1">
+          Discours acceuillants et
+          informels pour mieux comprendre notre
+          environnement</p>
       </div>
     </div>
   </section>
