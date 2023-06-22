1abe2607e50977c4e792dca77b7f9660319b8973
Max <max.lepan@laposte.net>
2023-06-22 17:52:35 +0200

    UPD [Footer] Fixed responsiveness

--- components/footer.vue
+++ components/footer.vue
@@ -4,14 +4,14 @@
 
 <template>
   <footer class="bg-olive-300 sm:p-36 p-5">
-    <div class="flex sm:flex-row flex-column justify-around items-center">
+    <div class="flex sm:flex-row flex-col sm:gap-0 gap-6 justify-around items-center">
       <img alt="small logo" class="h-10" src="/icons/small_logo.svg"/>
       <div class="flex flex-row gap-5">
         <img alt="instagram logo" class="h-10" src="/icons/ig_logo.svg"/>
         <img alt="facebook logo" class="h-10" src="/icons/facebook_logo.svg"/>
         <img alt="youtube logo" class="h-10" src="/icons/youtube_logo.svg"/>
       </div>
-      <div class="flex flex-row gap-4">
+      <div class="flex sm:flex-row flex-col gap-4">
         <a>
           <img alt="download on app store" src="/images/dl_appstore.png"/>
         </a>
