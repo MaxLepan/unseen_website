269517e32ffa02e9810bd0128dfa20a0cba7b481
Max <max.lepan@laposte.net>
2023-06-26 15:44:44 +0200

    FIX [Footer] Fixed partners images on responsive

--- components/smaller_components/ourPartners.vue
+++ components/smaller_components/ourPartners.vue
@@ -3,15 +3,15 @@
 </script>
 
 <template>
-  <div class="sm:px-52 px-8">
+  <div class="lg:px-52 sm:px-32 px-8">
     <h2 class="font-title italic text-2xl">{{ $t('ourPartners') }}</h2>
-    <div class="flex sm:flex-row flex-col sm:gap-0 gap-11 justify-between my-10 pb-6">
-      <img alt="Partenaire : Office National des Forêts" class="h-16 sm:object-cover object-contain"
+    <div class="flex md:flex-row flex-col md:gap-0 gap-11 justify-between my-10 pb-6">
+      <img alt="Partenaire : Office National des Forêts" class="h-16 md:object-cover object-contain"
            src="/images/onf.png">
-      <img alt="Partenaire : Les parcs nationaux de France" class="h-20 sm:object-cover object-contain"
+      <img alt="Partenaire : Les parcs nationaux de France" class="h-20 md:object-cover object-contain"
            src="/images/pnf.png">
-      <img alt="Partenaire : La République Française" class="h-20 sm:object-cover object-contain" src="/images/rf.png">
-      <img alt="Partenaire : L'Inventaire National du Patrimoine Naturel" class="h-14 sm:object-cover object-contain"
+      <img alt="Partenaire : La République Française" class="h-20 md:object-cover object-contain" src="/images/rf.png">
+      <img alt="Partenaire : L'Inventaire National du Patrimoine Naturel" class="h-14 md:object-cover object-contain"
            src="/images/inpn.png">
     </div>
   </div>
