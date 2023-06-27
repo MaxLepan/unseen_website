3e7a1522d22b4f3d1b687ea880128b0826df8f32
Max <max.lepan@laposte.net>
2023-06-27 15:37:04 +0200

    ADD [Animation] Added ant animation

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
+  <AntAnimation/>
   <div class="bg-beige-100 sm:p-16 sm:pt-8 p-0 pt-0 overflow-x-hidden">
     <NuxtPage/>
   </div>
--- components/landing.vue
+++ components/landing.vue
@@ -5,17 +5,17 @@
 <template>
   <section
       class="relative bg-beige-200 grid sm:grid-cols-2 gap-1 lg:px-20 lg:pt-0 sm:px-10 sm:pt-10 px-5 pt-5 sm:m-0 mx-5 rounded-lg h-full">
-    <div class="z-10 lg:pt-20 pt-0 lg:pb-20 sm:pb-10 pb-5 self-center">
-      <h1 class="font-bold font-title lg:text-5xl md:text-3xl text-5xl">{{ $t('headlineStart') }}<span
+    <div class="lg:pt-20 pt-0 lg:pb-20 sm:pb-10 pb-5 self-center">
+      <h1 class="font-bold font-title lg:text-5xl md:text-3xl text-5xl relative z-20">{{ $t('headlineStart') }}<span
           class="text-olive-400">{{
           $t('headlineColored')
         }}</span>{{ $t('headlineEnd') }}</h1>
-      <div class="flex flex-row gap-3.5 mt-4 mb-8">
+      <div class="relative flex flex-row gap-3.5 mt-4 mb-8">
         <img alt="Marker" src="/icons/place_01.svg">
-        <h2 class="font-title font-bold md:text-lg sm:text-base text-base">{{ $t('subHeadline') }}</h2>
+        <h2 class="font-title font-bold md:text-lg sm:text-base text-base relative z-20">{{ $t('subHeadline') }}</h2>
       </div>
       <button class="bg-olive-200 pt-2.5 pb-2.5 pr-4 pl-4 sm:w-auto w-full rounded-md">
-        <a class="font-body font-bold lg:text-2xl md:text-lg text-base" href="#">{{
+        <a class="font-body font-bold lg:text-2xl md:text-lg text-base relative z-20" href="#download">{{
             $t('downloadApp')
           }}</a>
       </button>
