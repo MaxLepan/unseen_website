f793640bd4a51a829516bcec185213e3dd19bff2
Max <max.lepan@laposte.net>
2023-06-27 14:01:37 +0200

    UPD [General] Changed team images and changed landing

--- components/landing.vue
+++ components/landing.vue
@@ -5,7 +5,7 @@
 <template>
   <section
       class="relative bg-beige-200 grid sm:grid-cols-2 gap-1 lg:px-20 lg:pt-0 sm:px-10 sm:pt-10 px-5 pt-5 sm:m-0 mx-5 rounded-lg h-full">
-    <div class="z-10 lg:pt-20 pt-0 lg:pb-20 sm:pb-10 pb-5">
+    <div class="z-10 lg:pt-20 pt-0 lg:pb-20 sm:pb-10 pb-5 self-center">
       <h1 class="font-bold font-title lg:text-5xl md:text-3xl text-5xl">{{ $t('headlineStart') }}<span
           class="text-olive-400">{{
           $t('headlineColored')
@@ -20,7 +20,7 @@
           }}</a>
       </button>
     </div>
-    <div class="w-full right-16 sm:block hidden">
+    <div class="w-3/4 justify-self-end sm:block hidden">
       <img alt="Application" class="z-10 relative" src="/images/mockup_fourmi.png">
     </div>
     <img alt="" class="absolute right-0 -top-24 sm:block hidden z-0" src="/icons/wave.svg">
