1c59c502ad80a29d4095a26c14a4b855dd66a895
Max <max.lepan@laposte.net>
2023-06-28 12:12:05 +0200

    FIX [Animation] Fixed z index

--- components/features.vue
+++ components/features.vue
@@ -1,17 +1,19 @@
 <template>
-  <section class="all-container relative bg-beige-200 py-24 sm:my-8 mb-28 mt-4 sm:px-28 px-5 sm:h-auto h-[210vh] relative">
-    <h2 class="font-title text-center font-bold lg:text-5xl md:text-4xl sm:text-3xl text-2xl sm:mb-20 mb-10">{{ $t('featureTitle') }}</h2>
+  <section
+      class="all-container relative bg-beige-200 py-24 sm:my-8 mb-28 mt-4 sm:px-28 px-5 sm:h-auto h-[210vh]">
+    <h2 class="font-title text-center font-bold lg:text-5xl md:text-4xl sm:text-3xl text-2xl sm:mb-20 mb-10 relative z-20">
+      {{ $t('featureTitle') }}</h2>
 
     <div class="flex flex-col-reverse lg:flex-row">
       <div class="lg:flex-1 screen-img">
         <div class="diff-img-container flex justify-center relative">
           <figure class="flex flex-row gap-32 w-80 mt-10 lg:mt-0 img-quiz absolute sm:relative">
-            <img alt="map" class="w-full basis-1/3 rounded-xl" :src="currentImage"/>
+            <img :src="currentImage" alt="map" class="w-full basis-1/3 rounded-xl"/>
           </figure>
         </div>
       </div>
 
-      <div class="lg:flex-1">
+      <div class="lg:flex-1 relative z-20">
         <div class="flex flex-row mb-5">
           <figure>
             <img alt="recherche" class="w-11 rounded-full bg-beige-100 p-1" src="../public/icons/recherche.svg"/>
@@ -21,24 +23,31 @@
 
         <div class="flex flex-col">
 
-          <div class="my-4 py-3.5 pl-5 lg:my-8 border-2 border-beige-300 flex flex-col items-start hover:bg-beige-300 duration-300" @mouseover="changeImage('/images/quiz_species.png')">
+          <div
+              class="my-4 py-3.5 pl-5 lg:my-8 border-2 border-beige-300 flex flex-col items-start hover:bg-beige-300 duration-300 relative z-20 hover:cursor-pointer"
+              @mouseover="changeImage('/images/quiz_species.png')">
             <h4 class="bg-green-100 py-1 px-3.5 mb-3.5 rounded">{{ $t('firstFeatureHeadLine') }}</h4>
             <div class="pr-16">{{ $t('firstFeaturetext') }}</div>
           </div>
 
-          <div class="my-4 py-3.5 pl-5 lg:my-8 border-2 border-beige-300 flex flex-col items-start hover:bg-beige-300 duration-300" @mouseover="changeImage('/images/quiz_place.png')">
+          <div
+              class="my-4 py-3.5 pl-5 lg:my-8 border-2 border-beige-300 flex flex-col items-start hover:bg-beige-300 duration-300 relative z-20 hover:cursor-pointer"
+              @mouseover="changeImage('/images/quiz_place.png')">
             <h4 class="bg-sapin-100 py-1 px-3.5 mb-3.5 rounded">{{ $t('secondFeatureHeadLine') }}</h4>
             <div class="pr-16">{{ $t('secondFeaturetext') }}</div>
           </div>
 
-          <div class="my-4 py-3.5 pl-5 lg:my-8 border-2 border-beige-300 flex flex-col items-start hover:bg-beige-300 duration-300" @mouseover="changeImage('/images/recherche.png')">
+          <div
+              class="my-4 py-3.5 pl-5 lg:my-8 border-2 border-beige-300 flex flex-col items-start hover:bg-beige-300 duration-300 relative z-20 hover:cursor-pointer"
+              @mouseover="changeImage('/images/recherche.png')">
             <h4 class="bg-orange-100 py-1 px-3.5 mb-3.5 rounded">{{ $t('thirdFeatureHeadLine') }}</h4>
             <div class="pr-16">{{ $t('thirdFeaturetext') }}</div>
           </div>
         </div>
       </div>
     </div>
-    <img alt="" class="md:visible invisible absolute rotate-[115deg] -right-[4%] -bottom-[6%] sm:block hidden z-0" src="/icons/wave.svg">
+    <img alt="" class="md:visible invisible absolute rotate-[115deg] -right-[4%] -bottom-[6%] sm:block hidden z-0"
+         src="/icons/wave.svg">
   </section>
 </template>
 
